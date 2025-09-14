75d1d7a899ba1d8faa02aa91f4c505bbc7d6b2ee
HassanIyan <personal.hassan.iyan.abdul.r@gmail.com>
2025-09-14 13:20:09 +0500

    few updates

--- src/app/(frontend)/[[...fragments]]/page.tsx
+++ src/app/(frontend)/[[...fragments]]/page.tsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, { cache } from 'react'
 import { getPayload } from 'payload'
 import config from '@payload-config'
 import { notFound } from 'next/navigation'
+import { Metadata } from 'next'
+import { ObjectifyMetaData } from '@/lib/utils'
+import { Metadatum } from '@/payload-types'
 
-export default async function page({ params }: { params: Promise<{ fragments: string[] }> }) {
-    const payload = await getPayload({ config })
-    const { fragments } = await params
-    const link = `/${fragments?.join('/') || ''}`
-
-    const pages = await payload.find({
-        collection: 'pages',
-        where: {
-            url: {
-                equals: link,
-            },
-        },
-    })
-
-    if (pages?.totalDocs === 0) return notFound()
-
-    return <main className="container">{link}</main>
+interface Props {
+    params: Promise<{ fragments: string[] }>
 }
+
+export default async function page({ params }: Props) {
+    const { fragments } = await params
+    const page = await getPage(fragments)
+
+    return <main className="container">{page.url}</main>
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { fragments } = await params
+    const { seo } = await getPage(fragments)
+
+    return ObjectifyMetaData(seo as Metadatum)
+}
+
+const getPage = cache(async (fragments: string[]) => {
+    try {
+        const link = `/${fragments?.join('/') || ''}`
+        const payload = await getPayload({ config })
+        const pages = await payload.find({
+            collection: 'pages',
+            where: {
+                url: {
+                    equals: link,
+                },
+            },
+        })
+        if (pages?.totalDocs === 0) return notFound()
+
+        return pages?.docs?.[0]
+    } catch (error: unknown) {
+        if ((error as Error).message === 'Not Found') return notFound()
+        throw error
+    }
+})
