bfee6f1f941a7f391c7a5ccb7ae7988ebfde3797
HassanIyan <personal.hassan.iyan.abdul.r@gmail.com>
2025-10-22 12:36:39 +0500

    added

--- src/seeders/layout.ts
+++ src/seeders/layout.ts
@@ -1,11 +1,12 @@
 import { getPayload } from 'payload'
 import config from '@payload-config'
-import data from '../../data/meta.json'
+import data from '../../data/layout.json'
 ;(async () => {
     const payload = await getPayload({ config })
 
     const layout = await payload.updateGlobal({
         slug: 'layout',
+        // @ts-expect-error: not typed
         data,
     })
 
